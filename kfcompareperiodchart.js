ca08920f3fabbaeacb29b46849a0b318f0d2fd65
karl.fredberg <karl.fredberg@adlibris.com>
2015-03-12 15:14:39 +0100

    Bug fixes

--- kfcompareperiodchart.js
+++ kfcompareperiodchart.js
@@ -1053,9 +1053,10 @@ define(["jquery", "text!./kfcompareperiodchart.css", "translator", "general.util
 
 		paint: function($element, layout) {
 
-			var dateFormatter = new numFormatter(this.backendApi.localeInfo, layout.qListObject.qDimensionInfo.qNumFormat.qFmt, layout.qListObject.qDimensionInfo.qNumFormat.qThou, layout.qListObject.qDimensionInfo.qNumFormat.qDec, 'D');
 
-			var tooltipFormatter = new numFormatter(this.backendApi.localeInfo, layout.tooltip.qNumberPresentations[0].qFmt, layout.tooltip.qNumberPresentations[0].qThou, layout.tooltip.qNumberPresentations[0].qDec, 'D');
+			var dateFormatter = new numFormatter(this.localeInfo, layout.qListObject.qDimensionInfo.qNumFormat.qFmt, layout.qListObject.qDimensionInfo.qNumFormat.qThou, layout.qListObject.qDimensionInfo.qNumFormat.qDec, 'D');
+
+			var tooltipFormatter = new numFormatter(this.localeInfo, layout.tooltip.qNumberPresentations[0].qFmt, layout.tooltip.qNumberPresentations[0].qThou, layout.tooltip.qNumberPresentations[0].qDec, 'D');
 
 			var errors = []
 
